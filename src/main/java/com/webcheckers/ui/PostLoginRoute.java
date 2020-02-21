f31b6ca0f6f0adbc043c29a01eca20ab279bf339
wymario <wymario@yahoo.com>
2020-02-21 15:56:56 -0500

    Refactored PostGuessRoute to be PostLoginRoute

--- src/main/java/com/webcheckers/ui/PostLoginRoute.java
+++ src/main/java/com/webcheckers/ui/PostLoginRoute.java
@@ -7,16 +7,16 @@ import spark.TemplateEngine;
 
 import java.util.logging.Logger;
 
-public class PostGuessRoute implements Route {
+public class PostLoginRoute implements Route {
 
-    private static final Logger LOG = Logger.getLogger(PostGuessRoute.class.getName());
+    private static final Logger LOG = Logger.getLogger(PostLoginRoute.class.getName());
 
     private static final String USER_USER = "username";
     private static final String USER_PASS = "password";
 
     private final TemplateEngine templateEngine;
 
-    public PostGuessRoute(final TemplateEngine templateEngine) {
+    public PostLoginRoute(final TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
         LOG.config("PostGuessRoute is initialized.");
     }
