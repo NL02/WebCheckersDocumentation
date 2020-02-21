ee223ff4ac1a0d60487754f826f89f1a8b238be7
wymario <wymario@yahoo.com>
2020-02-21 16:21:18 -0500

    Implemented constructor and ID getter for Player

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,5 +1,13 @@
 package com.webcheckers.model;
 
 public class Player {
+    private final int playerID; // Internal ID constructed from player's information
 
+    public Player(String username, String password) {
+        playerID = username.hashCode() + password.hashCode();
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
 }
