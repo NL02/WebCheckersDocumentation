963b5ba1a48eb5fee377e65bb9b38c76e5ac17e1
wymario <wymario@yahoo.com>
2020-02-21 16:28:03 -0500

    Added username field and getter for Player.

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,13 +1,25 @@
 package com.webcheckers.model;
 
 public class Player {
-    private final int playerID; // Internal ID constructed from player's information
+    private final int playerID; // Internal ID constructed by hashing player info
+    private final String username;
 
+    /**
+     * Construct a Player and set its fields.
+     *
+     * @param username User's name for logging in and display
+     * @param password User's password to log in with
+     */
     public Player(String username, String password) {
+        this.username = username;
         playerID = username.hashCode() + password.hashCode();
     }
 
     public int getPlayerID() {
         return playerID;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
