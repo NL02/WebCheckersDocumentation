844efc0c0a515b0c256273d376e8e93c663aa8aa
wymario <wymario@yahoo.com>
2020-02-23 16:12:44 -0500

    Initial implementations for BoardSquare, CheckersGame, and CheckersPiece.

--- src/main/java/com/webcheckers/model/BoardSquare.java
+++ src/main/java/com/webcheckers/model/BoardSquare.java
@@ -1,4 +1,32 @@
 package com.webcheckers.model;
 
 public class BoardSquare {
+    private Color color;
+    private CheckersPiece piece;
+
+    protected BoardSquare(Color c, CheckersPiece p) {
+        this.color = c;
+        this.piece = p;
+    }
+
+    protected BoardSquare(Color c) {
+        this(c, null);
+    }
+
+    public boolean hasPiece() {
+        return this.piece != null;
+    }
+
+    public void placePiece(CheckersPiece p) {
+        if (this.piece != null) {
+            this.removePiece();
+        }
+        this.piece = p;
+    }
+
+    public CheckersPiece removePiece() {
+        CheckersPiece copy = piece.duplicate();
+        this.piece = null;
+        return copy;
+    }
 }
--- src/main/java/com/webcheckers/model/CheckersPiece.java
+++ src/main/java/com/webcheckers/model/CheckersPiece.java
@@ -1,4 +1,31 @@
 package com.webcheckers.model;
 
 public class CheckersPiece {
+    private Color color;
+    private boolean isKing;
+
+    protected CheckersPiece(Color c) {
+        color = c;
+        isKing = false;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public boolean isKing() {
+        return this.isKing;
+    }
+
+    public void becomeKing() {
+        this.isKing = true;
+    }
+
+    public CheckersPiece duplicate() {
+        CheckersPiece copy = new CheckersPiece(this.color);
+        if (this.isKing) {
+            copy.becomeKing();
+        }
+        return copy;
+    }
 }
