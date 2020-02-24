72b0eec91e97d9b1f799a5bc0442dce87d603de6
Miao Lin Wu <mw4815@rit.edu>
2020-02-24 16:12:16 -0500

    added a Player class in util

--- src/main/java/com/webcheckers/ui/GetLoginRoute.java
+++ src/main/java/com/webcheckers/ui/GetLoginRoute.java
@@ -1,10 +1,15 @@
 package com.webcheckers.ui;
 
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.TemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 
 public class GetLoginRoute implements Route {
@@ -12,11 +17,16 @@ public class GetLoginRoute implements Route {
     private final TemplateEngine templateEngine;
 
     public GetLoginRoute(final TemplateEngine templateEngine) {
-        this.templateEngine = templateEngine;
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        //
     }
 
     public Object handle(Request request, Response response) {
-        return null;
+        Map<String, Object> vm = new HashMap<>();
+
+
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "home.ftl"));
     }
 
 }
