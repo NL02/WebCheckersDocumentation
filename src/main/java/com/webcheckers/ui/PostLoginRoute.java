79239af3e03896dd5d94f44eae9b4699dbbb3ed6
erd9091 <erd9091@rit.edu>
2020-02-24 16:49:38 -0500

    Added LOG output for debugging

--- src/main/java/com/webcheckers/ui/PostLoginRoute.java
+++ src/main/java/com/webcheckers/ui/PostLoginRoute.java
@@ -1,33 +1,33 @@
 package com.webcheckers.ui;
 
-import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.model.Player;
 import spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
 public class PostLoginRoute implements Route {
 
-    // Values used in the view-model map for rendering the post login view after a login attempt
+    // Values used in the view-model map for rendering the post login view after a login attemp
 
     private static final Logger LOG = Logger.getLogger(PostLoginRoute.class.getName());
-    private static final String USER_USER = "username";
 
-    private static final String ERROR_TYPE = "error";
+    static final String USER_USER = "username";
+    static final String USER_PASS = "password";
+    static final String MESSAGE_ATTR = "message";
 
+    static final String ERROR_TYPE = "error";
 
     //
     // Attributes
     //
     private final TemplateEngine templateEngine;
-    private final PlayerLobby playerLobby;
 
-    public PostLoginRoute( PlayerLobby playerLobby, TemplateEngine templateEngine) {
+    public PostLoginRoute( TemplateEngine templateEngine) {
         //validation
         Objects.requireNonNull(templateEngine,"templateEngine must not be null");
 
-        this.playerLobby = playerLobby;
         this.templateEngine = templateEngine;
         LOG.config("PostGuessRoute is initialized.");
     }
@@ -38,19 +38,15 @@ public class PostLoginRoute implements Route {
     @Override
     public Object handle(Request request, Response response) {
         // start building the View-Model
-        final Session httpSession = request.session();
+        final Map<String, Object> vm = new HashMap<>();
+
+
+        ModelAndView mv;
 
         final String username = request.queryParams(USER_USER);
-
-        // adds user to the map
-        boolean is_added = playerLobby.saveUser(username);
-        if(is_added){
-            PlayerLobby.increment();
-        }
-
-        httpSession.attribute("currentUser", new Player(username));
-
+        final String password = request.queryParams(USER_PASS);
         System.out.println(username);
+        System.out.println(password);
 
         response.redirect("/");
         return null;
