bb9ee437b7154e7c986282c3fd89a3e346cf5ea3
nelsonliang <nl3514@g.rit.edu>
2020-03-06 16:55:09 -0500

    Added Template Engine, PlayerLobby, and Logger to all the gameroutes

--- src/main/java/com/webcheckers/ui/gameroutes/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/gameroutes/PostSubmitTurnRoute.java
@@ -1,4 +1,21 @@
 package com.webcheckers.ui.gameroutes;
 
+import com.webcheckers.appl.PlayerLobby;
+import spark.TemplateEngine;
+
+import java.util.logging.Logger;
+
 public class PostSubmitTurnRoute {
+    private static final Logger LOG = Logger.getLogger(PostValidateMoveRoute.class.getName());
+
+    private final TemplateEngine templateEngine;
+    private final PlayerLobby playerLobby;
+
+    public PostSubmitTurnRoute(PlayerLobby playerLobby, final TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+        this.playerLobby = playerLobby;
+
+        LOG.config("PostSubmitturnRoute is initialized.");
+    }
+
 }
