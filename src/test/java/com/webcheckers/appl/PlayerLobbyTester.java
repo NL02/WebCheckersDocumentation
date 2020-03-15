5a9ba154e2cf23c05161ab795329ae95845c08d2
Tutorial <rvm8343@g.rit.edu>
2020-03-15 00:01:38 -0400

    Added a branch for Unit testing, outlined the tests I want to do for PlayerLobbyTester and PostSignOutRouteTester

--- src/test/java/com/webcheckers/appl/PlayerLobbyTester.java
+++ src/test/java/com/webcheckers/appl/PlayerLobbyTester.java
@@ -1,4 +1,227 @@
 package com.webcheckers.appl;
+import static org.junit.jupiter.api.Assertions.*;
 
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.webcheckers.appl.PlayerLobby;
+
+/**
+ * The unit test suite for the PlayerLobby component
+ *
+ * @author Rayna Mishra rvm8343
+ */
+@Tag("Application-tier")
 public class PlayerLobbyTester {
+
+    /**
+     * The component-under-test (CuT)
+     */
+    private PlayerLobby CuT;
+
+    // mock objects
+    private Player player1;
+    private Player player2;
+    private Player player3;
+
+    @BeforeEach
+    public void setup() {
+        CuT = new PlayerLobby();
+    }
+
+    /**
+     * Test the ability to make a new PlayerService
+     */
+    @Test
+    public void test_make_player_service(){
+        // Invoke test
+        final PlayerServices playerServices = CuT.newPlayerServices();
+        // check result
+        assertNotNull(playerServices);
+    }
+
+    /**
+     * Test the ability to make a new CheckersGame game
+     */
+    @Test
+    public void test_make_game(){
+        final PlayerLobby CuT = new PlayerLobby();
+        // Invoke test
+        final CheckersGame game = CuT.getGame("Player1");
+
+        // Check results
+        // 1) The returned game is not null
+        assertNotNull(game);
+        // 2) The whitePlayer is "Player1"
+        assertEquals("Player1", game.getWhitePlayer().getName());
+        // 3) The opponent is null
+        assertNull(game.getRedPlayer().getName());
+    }
+
+    /**
+     * Test the ability to increment the total number of games
+     */
+    @Test
+    public void test_game_finished(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to not save a user with an invalid user name -- no name given (null)
+     */
+    @Test
+    public void test_save_user_null_name(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to not save a user with an invalid user name -- name with invalid char
+     */
+    @Test
+    public void test_save_user_invalid_char_name(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to not save a user that gave a name that is already in use
+     */
+    @Test
+    public void test_save_user_need_new_name(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to save a user that has just logged in for the first time
+     */
+    @Test
+    public void test_save_user_new_name(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to save a user that has just logged in and is a returning player
+     */
+    @Test
+    public void test_save_user_returning_player(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to get the count of online players (live count)
+     */
+    @Test
+    public void test_get_live_count(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to increment the number of online players (live count)
+     */
+    @Test
+    public void test_increment(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to decrement the count of online players (live count)
+     */
+    @Test
+    public void test_decrement(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to get a list of waiting players when there are no waiting players
+     */
+    @Test
+    public void test_get_waiting_players_none(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to get a list of waiting players when there is one waiting player
+     */
+    @Test
+    public void test_get_waiting_players_one(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to get a list of waiting players when there are two waiting players
+     */
+    @Test
+    public void test_get_waiting_players_two(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to remove an online user from the list of online players
+     */
+    @Test
+    public void test_remove_user_online(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to remove an offline user from the list of online players
+     */
+    @Test
+    public void test_remove_user_offline(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to add a player to the list of online players
+     */
+    @Test
+    public void test_add_online_player(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to find a player in the user Map when the given player is null
+     */
+    @Test
+    public void test_find_player_null(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to find a player in the user Map when the player object is not in the map
+     */
+    @Test
+    public void test_find_player_non_existent(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test the ability to find a player in the user Map when the given player is valid
+     */
+    @Test
+    public void test_find_player_valid(){
+        // Invoke test
+        // Analyze results:
+    }
+
+
 }
--- src/test/java/com/webcheckers/ui/pageroutes/PostSignOutRouteTester.java
+++ src/test/java/com/webcheckers/ui/pageroutes/PostSignOutRouteTester.java
@@ -1,4 +1,113 @@
 package com.webcheckers.ui.pageroutes;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.ui.TemplateEngineTester;
+import com.webcheckers.ui.WebServer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import spark.*;
+
+/**
+ * The unit test suite for the PostSignOutTester
+ *
+ * @author Rayna Mishra rvm8343
+ */
+@Tag("UI-tier")
 public class PostSignOutRouteTester {
+
+    /**
+     * The component-under-test (CuT)
+     */
+    private PostSignOutRoute CuT;
+
+    // mock objects
+    private Request request;
+    private Session session;
+    private TemplateEngine engine;
+    private PlayerLobby lobby;
+    private Response response;
+
+    /**
+     * Setup new mock objects for each test.
+     */
+    @BeforeEach
+    public void setup(){
+       request = mock(Request.class);
+       session = mock(Session.class);
+       when(request.session()).thenReturn(session);
+       response = mock(Response.class);
+       lobby = mock(PlayerLobby.class);
+       engine = mock(TemplateEngine.class);
+
+       CuT = new PostSignOutRoute(lobby, engine);
+    }
+
+    /**
+     * Test ability to
+     */
+    @Test
+    public void test_new_sign_out(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render((any(ModelAndView.class)))).thenAnswer(testHelper.makeAnswer());
+
+        // Invoke test
+        CuT.handle(request, response);
+
+        // Analyze results:
+        //      * model is a non-null Map
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+        //      * model contains all necessary View-Model data
+        testHelper.assertViewModelAttribute(PostSignOutRoute.CURRENT_USER_ATTR, session.attribute("currentUser"));
+        //      * test view name
+        // todo: testHelper.assertViewName(PostSignOutRoute.VIEWNAME);
+        //  also, do we need a timeout session key here?
+    }
+
+    /**
+     * Test ability to remove a player from the online players list given a player that was online
+     */
+    @Test
+    public void removePlayerWasOnline(){
+        // Invoke test
+        // Analyze results:
+    }
+
+    /**
+     * Test ability to remove a player from the online players list given a player that is
+     * not online
+     */
+    @Test
+    public void removePlayerWasNotOnline(){
+
+    }
+
+    /**
+     * Test ability to set the current player's status to offline
+     */
+    @Test
+    public void setStatusOFFLINE(){
+
+    }
+
+    /**
+     * Test ability to remove the current player from the session attribute
+     */
+    @Test
+    public void removePlayerFromSession(){
+
+    }
+
+    /**
+     * Test that CuT redirects to the home page after a player signs out (at the end of the sign
+     * out routine)
+     */
+    @Test
+    public void redirectToHome(){
+        CuT.handle(request, response);
+        verify(response).redirect(WebServer.HOME_URL);
+    }
 }
