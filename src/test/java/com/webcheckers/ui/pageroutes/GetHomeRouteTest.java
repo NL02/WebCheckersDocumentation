74f2bf4afa5e25f1a70d916a6f3b1ae3ffa3edf8
nelsonliang <nl3514@g.rit.edu>
2020-03-25 14:42:30 -0400

    GetHomeRouteTest

--- src/test/java/com/webcheckers/ui/pageroutes/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/pageroutes/GetHomeRouteTest.java
@@ -1,18 +1,15 @@
 package com.webcheckers.ui.pageroutes;
 
 import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.appl.PlayerServices;
 import com.webcheckers.ui.TemplateEngineTester;
-
-import com.webcheckers.ui.WebServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import spark.*;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * The unit test suite for the PostSignOutTester
@@ -43,59 +40,26 @@ public class GetHomeRouteTest {
         lobby = mock(PlayerLobby.class);
         engine = mock(TemplateEngine.class);
 
-
         CuT = new GetHomeRoute(lobby, engine);
     }
 
+
     /**
-     * Test that CuT shows the Home view when the session is brand new.
+     * Test the handle() method to see if it renders the home page
      */
     @Test
-    public void new_session(){
-        // To analyze what the Route created in the View-Model map you need
-        // to be able to extract the argument to the TemplateEngine.render method.
-        // Mock up the 'render' method by supplying a Mockito 'Answer' object
-        // that captures the ModelAndView data passed to the template engine
-        final TemplateEngineTester testHelper = new TemplateEngineTester();
-        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    public void testRender() {
+        final TemplateEngineTester engineTester = new TemplateEngineTester();
+        when(engine.render((any(ModelAndView.class)))).thenAnswer(engineTester.makeAnswer());
 
-        // Invoke the test
+        // Render the page
         CuT.handle(request, response);
 
-        // Analyze the results:
-        // * model is a non-null Map
-        testHelper.assertViewModelExists();
-        testHelper.assertViewModelIsaMap();
-        // * model contains all necessary View-Model data
-        testHelper.assertViewModelAttribute(GetHomeRoute.TITLE_ATTR,GetHomeRoute.TITLE);
-        testHelper.assertViewModelAttribute(GetHomeRoute.CURRENT_USER_ATTR, session.attribute("currentUser"));
-        testHelper.assertViewModelAttribute(GetHomeRoute.NEW_PLAYER_ATTR, Boolean.TRUE);
-        // * test view name
-        testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
-        // * verify that a player service object and the session timeout watchdog are stored
-        // * in the session
-        verify(session).attribute(eq(GetHomeRoute.PLAYERSERVICES_KEY), any(PlayerServices.class));
-        verify(session).attribute(eq(GetHomeRoute.TIMEOUT_SESSION_KEY), any(SessionTimeoutWatchdog.class));
-    }
-
-    /**
-     * Test that CuT redirects to the Game view when a PlayerServices object exists.
-     */
-    @Test
-    public void old_session() {
-        // Arrange the test scenario: There is an existing session
-        when(session.attribute(GetHomeRoute.PLAYERSERVICES_KEY)).thenReturn(lobby.newPlayerServices());
-
-        // Invoke the test
-        try {
-            CuT.handle(request, response);
-            fail("Redirects invoke halt exceptions.");
-        } catch (HaltException e){
-            // expected
-        }
-        // Analyze the results:
-        // * redirect to the Game view
-        verify(response).redirect(WebServer.GAME_URL);
+//         Assert that the view-model is a non-null Map with the proper attributes and view name
+        engineTester.assertViewModelExists();
+        engineTester.assertViewModelIsaMap();
+        engineTester.assertViewModelAttribute(GetHomeRoute.TITLE, session.attribute(GetHomeRoute.TITLE_ATTR));
+        engineTester.assertViewName(GetHomeRoute.VIEW_NAME);
     }
 
 
