be5e679786fded3d809bd5acf84259bcda5c6fa8
Tutorial <rvm8343@g.rit.edu>
2020-04-01 16:39:17 -0400

    created the PostResignRoute and started putting in basic info

--- src/main/java/com/webcheckers/ui/gameroutes/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/gameroutes/PostValidateMoveRoute.java
@@ -1,6 +1,14 @@
 package com.webcheckers.ui.gameroutes;
 
+import com.google.gson.Gson;
 import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Move;
+import com.webcheckers.ui.pageroutes.PostSignOutRoute;
+import com.webcheckers.util.Message;
+import spark.Request;
+import spark.Response;
+import spark.Session;
 import spark.TemplateEngine;
 
 import java.util.logging.Logger;
@@ -10,12 +18,28 @@ public class PostValidateMoveRoute {
 
     private final TemplateEngine templateEngine;
     private final PlayerLobby playerLobby;
+    static final String CURRENT_USER_ATTR = "currentUser";
 
     public PostValidateMoveRoute(PlayerLobby playerLobby, final TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
         this.playerLobby = playerLobby;
 
         LOG.config("PostValidateMoveRoute is initialized.");
+    }
+
+    public Object handle(Request request, Response response) throws Exception{
+        final Session httpSession = request.session();
+        Gson gson = new Gson();
+        Move move = gson.fromJson(request.body(), Move.class);
+
+        //String currUser = httpSession.attribute(PostSignOutRoute.CURRENT_USER_ATTR);
+        //CheckersGame game = PlayerLobby.getGame(currUser);
+
+        Message moveMessage = Message.info("valid");
+
+        String json;
+        json = gson.toJson(moveMessage);
+        return json;
     }
 
 }
---
+++ src/main/java/com/webcheckers/ui/pageroutes/PostResignRoute.java
@@ -0,0 +1,35 @@
+package com.webcheckers.ui.pageroutes;
+
+import com.google.gson.internal.$Gson$Preconditions;
+import com.webcheckers.appl.PlayerLobby;
+import spark.Request;
+import spark.Response;
+import spark.TemplateEngine;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class PostResignRoute {
+
+    private static final Logger LOG = Logger.getLogger(PostResignRoute.class.getName());
+    //
+    // Attributes
+    //
+    private final PlayerLobby playerLobby;
+    private final TemplateEngine templateEngine;
+
+    public PostResignRoute( PlayerLobby playerLobby, TemplateEngine templateEngine){
+
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+
+        this.playerLobby = playerLobby;
+        this.templateEngine = templateEngine;
+        LOG.config("PostResignRoute is initialized");
+    }
+
+    @Override
+    public Object handle(Request request, Response response){
+
+    }
+
+}
