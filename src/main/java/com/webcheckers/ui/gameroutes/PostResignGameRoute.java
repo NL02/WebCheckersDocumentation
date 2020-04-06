de8cd2f6026869d24567b2b5a90edc728260b082
erd9091 <erd9091@rit.edu>
2020-04-06 14:41:20 -0400

    Fixed errors from merging to master

--- src/main/java/com/webcheckers/ui/gameroutes/PostResignGameRoute.java
+++ src/main/java/com/webcheckers/ui/gameroutes/PostResignGameRoute.java
@@ -37,9 +37,6 @@ public class PostResignGameRoute implements Route {
         Gson gson = gsonBuilder.create();
         Game game = me.getGame();
         game.gameOver(String.format(GAME_OVER_MSG, me.name), game.getRedPlayer() == me ? game.getWhitePlayer() : game.getRedPlayer());
-        if(game.getActivePlayer() != me) {
-            game.changeActiveColor();
-        }
 
         return gson.toJson(Message.info("true"));
     }
