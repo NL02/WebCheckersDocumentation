6ee984e5260980e14829798a516f45c51f732cc6
Miao Lin Wu <mw4815@rit.edu>
2020-04-10 16:02:26 -0400

    Unit tests for Move, Piece, Position and Space. All should pass.

--- src/test/java/com/webcheckers/model/MoveTest.java
+++ src/test/java/com/webcheckers/model/MoveTest.java
@@ -1,9 +1,68 @@
 package com.webcheckers.model;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit test for Move class.
+ *
+ * Author: May Wu
+ */
 @Tag("Model-tier")
 public class MoveTest {
+
+    /**
+     * The component-under-test (CuT)
+     */
+    private Move CuT;
+
+    @BeforeEach
+    public void setup() {
+        Position start = new Position(0, 0);
+        Position end = new Position(1,1);
+        CuT = new Move(start, end);
+    }
+
+    @Test
+    public void ctor_exists() {
+        assertNotNull(CuT);
+    }
+
+    @Test
+    public void test_isValid_true() {
+        assertTrue(CuT.isValid(), "Move is valid");
+    }
+
+    @Test
+    public void test_isValid_false() {
+        Move invalidStart = new Move(new Position(-1, -1), new Position(0, 0));
+        assertFalse(invalidStart.isValid(), "Move is not valid");
+        Move invalidEnd = new Move(new Position(5, 6), new Position(8, 8));
+        assertFalse(invalidEnd.isValid(), "Move is not valid");
+    }
+
+    @Test
+    public void test_getStart() {
+        assertEquals(0, CuT.getStart().getRow(), "Incorrect start position row");
+        assertEquals(0, CuT.getStart().getCell(), "Incorrect start position cell");
+    }
+
+    @Test
+    public void test_getEnd() {
+        assertEquals(1, CuT.getEnd().getRow(), "Incorrect end position row");
+        assertEquals(1, CuT.getEnd().getCell(), "Incorrect end position cell");
+    }
+
+
+    @Test
+    public void test_getMidpoint() {
+        CuT.setMidpoint();
+        assertEquals(0, CuT.getMidpoint().getRow(), "Incorrect midpoint position row");
+        assertEquals(0, CuT.getMidpoint().getCell(), "Incorrect midpoint position cell");
+    }
+
+
 }
--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -1,9 +1,48 @@
 package com.webcheckers.model;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit test for Piece class.
+ *
+ * Author: May Wu
+ */
 @Tag("Model-tier")
 public class PieceTest {
+
+    /**
+     * The component-under-test (CuT)
+     */
+    private Piece CuT;
+
+    @BeforeEach
+    public void setup() {
+        CuT = new Piece(Color.RED);
+    }
+
+    @Test
+    public void ctor_Arg() {
+        assertNotNull(CuT);
+    }
+
+    @Test
+    public void test_getColor() {
+        assertEquals(Color.RED, CuT.getColor(), "Piece is red");
+    }
+
+    @Test
+    public void test_getType() {
+        assertEquals(Piece.PieceType.SINGLE, CuT.getType(), "Piece is single");
+    }
+
+    @Test
+    public void test_king() {
+        CuT.kingMe();
+        assertEquals(Piece.PieceType.KING, CuT.getType(), "Piece is king");
+    }
+
 }
