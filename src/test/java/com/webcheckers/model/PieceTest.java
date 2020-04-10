12e2ae163ba9b10f497ef96a2cd2d98848c0cbae
Miao Lin Wu <mw4815@rit.edu>
2020-04-10 15:20:08 -0400

    Imported necessary classes and tagged the Model classes

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -1,4 +1,9 @@
 package com.webcheckers.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Model-tier")
 public class PieceTest {
 }
--- src/test/java/com/webcheckers/model/PositionTest.java
+++ src/test/java/com/webcheckers/model/PositionTest.java
@@ -1,4 +1,11 @@
 package com.webcheckers.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Model-tier")
 public class PositionTest {
+
+
 }
