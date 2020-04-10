d4423df4c20c056ff01496e98ce308d2a4be087f
nelsonliang <nl3514@g.rit.edu>
2020-04-10 17:54:42 -0400

    Added some tests for board.

--- src/test/java/com/webcheckers/model/BoardTest.java
+++ src/test/java/com/webcheckers/model/BoardTest.java
@@ -1,9 +1,52 @@
 package com.webcheckers.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.mock;
+
+/**
+ *  Unit test for Board class
+ *
+ *  Author:
+ */
 @Tag("Model-tier")
 public class BoardTest {
+
+    /**
+     * The component-under-test (CuT)
+     */
+
+    private Board CuT;
+
+    // attributes holding mock objects
+    private Space[][] anotherBoard;
+
+    @BeforeEach
+    public void setup() {
+        CuT = new Board();
+    }
+
+    @Test
+    public void initColor() {
+        assertEquals(Color.RED,CuT.getActiveColor());
+    }
+
+    @Test
+    public void changeColor() {
+        CuT.changeActiveColor();
+        assertEquals(Color.WHITE,CuT.getActiveColor());
+    }
+
+    @Test
+    public void gettingBoard() {
+        anotherBoard = mock(Space[][].class);
+        assertSame(anotherBoard, CuT.getBoard());
+    }
+
+
+
 }
